6f56907aded41e55f38f1150799234d16a75e1aa
Thibault Deutsch <thibault@arista.com>
2025-07-31 19:40:40 +0200

    Testing active healthcheck with mTLS
    Seems to not be working, the healthcheck metric says "network failure".
    Need investigation.
    
    AI_ASSISTED

--- main.go
+++ main.go
@@ -1,13 +1,13 @@
 // main.go
 //
-// This Go application implements a gRPC server with a health checking service
-// and a streaming endpoint that sends the current time. The health status
-// can be toggled via an HTTP endpoint.
+// This Go application implements a gRPC server with mTLS enabled.
+// It requires and verifies client certificates signed by the trusted CA.
 
 package main
 
 import (
-	"flag"
+	"crypto/tls"
+	"crypto/x509"
 	"fmt"
 	"log"
 	"net"
@@ -17,6 +17,7 @@ import (
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/credentials"
 	"google.golang.org/grpc/health"
 	"google.golang.org/grpc/health/grpc_health_v1"
 	"google.golang.org/grpc/reflection"
@@ -25,15 +26,13 @@ import (
 	pb "github.com/dethi/envoy_hck/protos"
 )
 
-// server is used to implement the TimeServiceServer.
 type server struct {
 	pb.UnimplementedTimeServiceServer
 }
 
-// StreamTime sends the current time to the client every 10 seconds.
 func (s *server) StreamTime(req *pb.TimeRequest, stream pb.TimeService_StreamTimeServer) error {
 	log.Println("StreamTime request received")
-	ticker := time.NewTicker(1 * time.Second)
+	ticker := time.NewTicker(10 * time.Second)
 	defer ticker.Stop()
 
 	for {
@@ -52,42 +51,47 @@ func (s *server) StreamTime(req *pb.TimeRequest, stream pb.TimeService_StreamTim
 }
 
 var (
-	// Mutex to protect access to the health status
-	mu sync.Mutex
-	// The current health status of the service
+	mu        sync.Mutex
 	isHealthy = true
 )
 
 func main() {
-	// Define command-line flags for ports
-	grpcPort := flag.Int("grpc-port", 50051, "The gRPC server port")
-	httpPort := flag.Int("http-port", 8081, "The HTTP server port for health toggling")
-	flag.Parse()
+	// --- Load TLS credentials ---
+	serverCert, err := tls.LoadX509KeyPair("certs/server.crt", "certs/server.key")
+	if err != nil {
+		log.Fatalf("failed to load server cert: %v", err)
+	}
+
+	caCert, err := os.ReadFile("certs/ca.crt")
+	if err != nil {
+		log.Fatalf("failed to read ca cert: %v", err)
+	}
+	caCertPool := x509.NewCertPool()
+	caCertPool.AppendCertsFromPEM(caCert)
+
+	tlsConfig := &tls.Config{
+		Certificates: []tls.Certificate{serverCert},
+		ClientCAs:    caCertPool,
+		ClientAuth:   tls.RequireAndVerifyClientCert, // Require clients to present a cert from our CA
+	}
+
+	creds := credentials.NewTLS(tlsConfig)
 
 	// --- gRPC Server ---
-	grpcAddr := fmt.Sprintf(":%d", *grpcPort)
-	lis, err := net.Listen("tcp", grpcAddr)
+	lis, err := net.Listen("tcp", ":50051")
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
-	s := grpc.NewServer()
+	s := grpc.NewServer(grpc.Creds(creds)) // Apply TLS credentials to the server
 
-	// Register the TimeService
 	pb.RegisterTimeServiceServer(s, &server{})
-
-	// Register the health service.
 	healthServer := health.NewServer()
 	grpc_health_v1.RegisterHealthServer(s, healthServer)
-
-	// Register reflection service on gRPC server.
 	reflection.Register(s)
-
-	// Set the initial health status for our service.
-	// An empty service name means it applies to the entire server.
 	healthServer.SetServingStatus("", grpc_health_v1.HealthCheckResponse_SERVING)
 
 	go func() {
-		log.Println("gRPC server listening at", lis.Addr())
+		log.Println("gRPC server with mTLS listening at", lis.Addr())
 		if err := s.Serve(lis); err != nil {
 			log.Fatalf("failed to serve: %v", err)
 		}
@@ -97,9 +101,7 @@ func main() {
 	http.HandleFunc("/toggle-health", func(w http.ResponseWriter, r *http.Request) {
 		mu.Lock()
 		defer mu.Unlock()
-
 		isHealthy = !isHealthy
-
 		var statusString string
 		if isHealthy {
 			healthServer.SetServingStatus("", grpc_health_v1.HealthCheckResponse_SERVING)
@@ -108,14 +110,12 @@ func main() {
 			healthServer.SetServingStatus("", grpc_health_v1.HealthCheckResponse_NOT_SERVING)
 			statusString = "UNHEALTHY"
 		}
-
 		log.Printf("Health status toggled to: %s", statusString)
 		fmt.Fprintf(w, "Health status is now %s\n", statusString)
 	})
 
-	httpAddr := fmt.Sprintf(":%d", *httpPort)
-	log.Printf("Health toggle server listening at %s", httpAddr)
-	if err := http.ListenAndServe(httpAddr, nil); err != nil {
+	log.Println("Health toggle server listening at :8081")
+	if err := http.ListenAndServe(":8081", nil); err != nil {
 		log.Fatalf("failed to start HTTP server: %v", err)
 	}
 }
